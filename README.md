793df27cfd289495d37e86a664205f07e4772a7f
Artem Yadelskyi <mymmrac@gmail.com>
2022-07-23 14:57:09 +0300

    Added handler ordering example

--- README.md
+++ README.md
@@ -76,6 +76,7 @@ More examples can be seen here:
 - [Inline query bot](examples/inline_query_bot/main.go)
 - [Bot handlers](examples/handler/main.go)
 - [Custom predicates for handlers](examples/handler_custom/main.go)
+- [Handler ordering](examples/handler_ordering/main.go)
 - [Specific handlers](examples/handler_specific/main.go)
 - [Predicate as middleware](examples/middleware_with_predicates/main.go)
 - [Update processor](examples/update_processor/main.go)
---
+++ examples/handler_ordering/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+)
+
+func main() {
+	botToken := os.Getenv("TOKEN")
+
+	// Note: Please keep in mind that default logger may expose sensitive information, use in development only
+	bot, err := telego.NewBot(botToken, telego.WithDefaultLogger(true, true))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	updates, _ := bot.UpdatesViaLongPulling(nil)
+	defer bot.StopLongPulling()
+
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	// Should not be here, because order of handlers do meter.
+	//
+	// bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+	// 	 fmt.Println("Message:", message.Text)
+	// })
+	//
+	// When you are defining handlers only first matched handler will process update, that means that in this specific
+	// example matching any message will "shadow" any other handler that matches on messages. For any message it will
+	// match and handling of start command will never happen.
+	//
+	// General idea is that you should define most specific handlers first (like for specific command or etc.) and only
+	// then more generic handlers (like any message).
+
+	// Will match any message with command `/start`
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		fmt.Println("Start")
+	}, th.CommandEqual("start"))
+
+	// Will match to any message
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		fmt.Println("Message:", message.Text)
+	})
+}
