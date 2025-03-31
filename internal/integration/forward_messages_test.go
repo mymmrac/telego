1ec9b80c50e4841a9f16b53bff3432f6dae3d1e4
sinaw369 <sinahatami1379@gmail.com>
2025-03-31 19:50:58 +0330

    fix: Cannot call a pointer method ChatID

--- internal/integration/forward_messages_test.go
+++ internal/integration/forward_messages_test.go
@@ -1,6 +1,6 @@
 //go:build integration && interactive
 
-package integration
+package main
 
 import (
 	"context"
--- internal/integration/send_test.go
+++ internal/integration/send_test.go
@@ -1,6 +1,6 @@
 //go:build integration
 
-package integration
+package main
 
 import (
 	"context"
