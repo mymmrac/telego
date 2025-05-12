1cb25a7bbb63cf9b21b2b30a2175f3c590e4fa76
Artem Yadelskyi <mymmrac@gmail.com>
2025-05-12 12:39:11 +0300

    Added fiber example

--- examples/go.mod
+++ examples/go.mod
@@ -3,6 +3,7 @@ module github.com/mymmrac/telego/examples
 go 1.23.4
 
 require (
+	github.com/gofiber/fiber/v3 v3.0.0-beta.4
 	github.com/mymmrac/telego v1.0.2
 	github.com/valyala/fasthttp v1.62.0
 	golang.ngrok.com/ngrok v1.13.0
@@ -13,7 +14,11 @@ require (
 	github.com/bytedance/sonic v1.13.1 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gofiber/schema v1.2.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/grbit/go-json v0.11.0 // indirect
 	github.com/inconshreveable/log15 v3.0.0-testing.5+incompatible // indirect
 	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
@@ -22,16 +27,21 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fastjson v1.6.4 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.ngrok.com/muxado/v2 v2.0.1 // indirect
 	golang.org/x/arch v0.6.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ examples/multi_bot_webhook_fiber/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+
+	"github.com/mymmrac/telego"
+)
+
+func main() {
+	ctx := context.Background()
+	botToken1 := os.Getenv("TOKEN_1")
+	botToken2 := os.Getenv("TOKEN_2")
+
+	// Create multiple bots with different tokens
+	// Note: Please keep in mind that default logger may expose sensitive information, use in development only
+	bot1, _ := telego.NewBot(botToken1, telego.WithDefaultDebugLogger())
+	bot2, _ := telego.NewBot(botToken2, telego.WithDefaultDebugLogger())
+
+	// Set up a webhook on Telegram side for each bot with different URLs
+	_ = bot1.SetWebhook(ctx, &telego.SetWebhookParams{
+		URL:         "https://example.com/bot1",
+		SecretToken: bot1.SecretToken(),
+	})
+	_ = bot2.SetWebhook(ctx, &telego.SetWebhookParams{
+		URL:         "https://example.com/bot2",
+		SecretToken: bot2.SecretToken(),
+	})
+
+	// Create a common webhook serve mux (or another custom server) for all bots
+	app := fiber.New()
+
+	// Get updates chan from webhook with respect to webhook URL
+	// Note: Each bot should use the same webhook serve mux (or another custom server)
+	updates1, _ := bot1.UpdatesViaWebhook(ctx, WebhookFiber(app, "/bot1", bot1.SecretToken()))
+	updates2, _ := bot2.UpdatesViaWebhook(ctx, WebhookFiber(app, "/bot2", bot2.SecretToken()))
+
+	// Start server for receiving requests from the Telegram
+	go func() { _ = app.Listen(":443") }()
+
+	// Loop through all updates when they came
+	go func() {
+		for update := range updates1 {
+			fmt.Printf("Update 1: %+v\n", update)
+		}
+	}()
+	for update := range updates2 {
+		fmt.Printf("Update 2: %+v\n", update)
+	}
+}
+
+// WebhookFiber returns a fiber handler for update webhook
+func WebhookFiber(router fiber.Router, path string, secretToken string) func(handler telego.WebhookHandler) error {
+	return func(handler telego.WebhookHandler) error {
+		router.Post(path, func(fCtx fiber.Ctx) error {
+			if secretToken != string(fCtx.Request().Header.Peek(telego.WebhookSecretTokenHeader)) {
+				return fCtx.SendStatus(fiber.StatusUnauthorized)
+			}
+
+			if err := handler(fCtx.Context(), fCtx.Body()); err != nil {
+				return fCtx.SendStatus(fiber.StatusInternalServerError)
+			}
+
+			return fCtx.SendStatus(fiber.StatusOK)
+		})
+		return nil
+	}
+}
